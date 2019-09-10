0182b2d334d02012478f32f5255777f7b3ff0010
Jack Daniel Kinne <bravelemming@gmail.com>
2019-09-09 22:05:09 -0700

    status; overflow on array out of bounds error.  have test.

--- code401challenges/src/test/java/code401challenges/MergeSortTest.java
+++ code401challenges/src/test/java/code401challenges/MergeSortTest.java
@@ -9,10 +9,14 @@ public class MergeSortTest {
     @Test
     public void mergeSort() {
 
+        //array
         int test[] = {456, 56, 123, 546, 0, 1};
 
         MergeSort.mergeSort(test);
 
         MergeSort.toString(test);
+
+        int expected[] = {0, 1, 56, 123, 456, 546};
+        assertArrayEquals(expected, test);
     }
 }
